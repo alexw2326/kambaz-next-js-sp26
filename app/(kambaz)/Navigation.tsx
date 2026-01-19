2e06f17828b8ab0d42d6d4499522e5352d5b07f9
Alexandria Wright <wright.alex@northeastern.edu>
2026-01-19 15:54:27 -0500

    changed the account nav item to go to profile

--- app/(kambaz)/Navigation.tsx
+++ app/(kambaz)/Navigation.tsx
@@ -3,7 +3,7 @@ export default function KambazNavigation() {
   return (
     <div id="wd-kambaz-navigation">
       <a href="https://www.northeastern.edu/" id="wd-neu-link" target="_blank">Northeastern</a><br/>
-      <Link href="/account" id="wd-account-link">Account</Link><br/>
+      <Link href="/account/profile" id="wd-account-link">Account</Link><br/>
       <Link href="/dashboard" id="wd-dashboard-link">Dashboard</Link><br/>
       <Link href="/dashboard" id="wd-course-link">Courses</Link><br/>
       <Link href="/calendar" id="wd-calendar-link">Calendar</Link><br/>
