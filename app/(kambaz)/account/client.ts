f078bd149e887e8a634747bd98af00ab1d6b4abc
Alexandria Wright <wright.alex@northeastern.edu>
2026-04-08 11:45:34 -0400

    accidentally started a6 on here, making new branch now

--- app/(kambaz)/account/client.ts
+++ app/(kambaz)/account/client.ts
@@ -9,7 +9,7 @@ export const signin = async (credentials: any) => {
 };
 export const profile = async () => {
   try {
-    const response = await axiosWithCredentials.post(`${USERS_API}/profile`);
+    const response = await axiosWithCredentials.get(`${USERS_API}/profile`);
     return response.data;
   } catch (err: any) {
     if (err.response?.status === 401) {
@@ -26,7 +26,32 @@ export const signout = async () => {
   const response = await axiosWithCredentials.post(`${USERS_API}/signout`);
   return response.data;
 };
+export const findAllUsers = async () => {
+  const response = await axiosWithCredentials.get(USERS_API);
+  return response.data;
+};
+export const findUsersByRole = async (role: string) => {
+  const response = await
+    axios.get(`${USERS_API}?role=${role}`);
+  return response.data;
+};
+export const findUsersByPartialName = async (name: string) => {
+  const response = await axios.get(`${USERS_API}?name=${name}`);
+  return response.data;
+};
+export const findUserById = async (id: string) => {
+  const response = await axios.get(`${USERS_API}/${id}`);
+  return response.data;
+};
+export const deleteUser = async (userId: string) => {
+  const response = await axios.delete( `${USERS_API}/${userId}` );
+  return response.data;
+};
 export const updateUser = async (user: any) => {
   const response = await axiosWithCredentials.put(`${USERS_API}/${user._id}`, user);
+  return response.data;
+};
+export const createUser = async (user: any) => {
+  const response = await axios.post(`${USERS_API}`, user);
   return response.data;
 };
