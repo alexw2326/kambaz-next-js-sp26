2c3ea303fa730cedf60ef3d0819ed393e9ad7e56
Alexandria Wright <wright.alex@northeastern.edu>
2026-04-16 14:05:23 -0400

    hopefully fixing axios error this time

--- app/(kambaz)/account/client.ts
+++ app/(kambaz)/account/client.ts
@@ -2,7 +2,7 @@
 import axios from "axios";
 const axiosWithCredentials = axios.create({
   withCredentials: true,
-  baseURL: process.env.NEXT_PUBLIC_REMOTE_SERVER,
+  baseURL: process.env.NEXT_PUBLIC_HTTP_SERVER,
 });
 export const HTTP_SERVER = process.env.NEXT_PUBLIC_HTTP_SERVER;
 export const USERS_API = `${HTTP_SERVER}/api/users`;
--- app/(kambaz)/courses/client.ts
+++ app/(kambaz)/courses/client.ts
@@ -1,6 +1,9 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
 import axios from "axios";
-const axiosWithCredentials = axios.create({ withCredentials: true });
+const axiosWithCredentials = axios.create({
+  withCredentials: true,
+  baseURL: process.env.NEXT_PUBLIC_HTTP_SERVER,
+});
 const HTTP_SERVER = process.env.NEXT_PUBLIC_HTTP_SERVER;
 const USERS_API = `${HTTP_SERVER}/api/users`;
 const COURSES_API = `${HTTP_SERVER}/api/courses`;
